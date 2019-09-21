3eb5f84a3cac4b981a1d16a13e0a5ad90365a219
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-09-21 21:36:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,13 @@
 def new_hash
-  # return an empty hash
+ return hash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  hash[:key] = 1
+  p "-" * 10
+  p hash[:key]
+  p "-" * 10
 end
 
 def pioneer
